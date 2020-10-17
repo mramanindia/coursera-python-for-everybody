9fb0791b18961441534f6d00bd664091310f7669
amanindiamuzz <69667533+amanindiamuzz@users.noreply.github.com>
2020-10-18 01:03:23 +0530

    Create 8_4_filemanagement.py

---
+++ 8_4_filemanagement.py
@@ -0,0 +1,19 @@
+#Open the file romeo.txt and read it line by line. For each line, split the line into a list of words using the split() method. 
+#The program should build a list of words. For each word on each line check to see if the word is already in the list and if not append it to the list. 
+#When the program completes, sort and print the resulting words in alphabetical order.
+
+fh = open('romeo.txt')
+lst = list()
+for line in fh:
+    word=line.rstrip()
+    word=line.split()
+    for element in word:
+        if(element not in lst):
+            lst.append(element)
+lst.sort()
+print(lst)
+    
+
+
+      
+   
